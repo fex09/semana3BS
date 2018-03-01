b8786ac18bb22fd8d83d98075f8c9868e6f9d970
Fenix <fsolisg@gmail.com>
2018-02-28 20:31:55 -0600

    nuevo commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,33 @@
 var gulp=require('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    sass = require('gulp-sass');
+    runSequence = require('run-sequence');
 
 gulp.task('server',function(){
     connect.server({
         port: 8888,
-        root:'./'
+        root:'./',
+        livereaload:true
     });
 });
 
-gulp.task('my-task',function(){
-    console.log('my task!');
+gulp.task('sass',function(){
+    return gulp.src('./scss/main.scss')
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }).on('error',sass.logError))
+    .pipe(gulp.dest('./style'))
+    .pipe(connect.reload());
+});
+
+gulp.task('watch',function(){
+    gulp.watch('./scss/**/*.scss',['sass']);
+});
+
+gulp.task('default',function(){
+    runSequence(
+        'sass',
+        'server',
+        'watch'
+    );
 });
